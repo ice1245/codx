a1146d87fc1d0d1b8c4089011e52e3836c2ee0d3
Gustavo Brian <gustavo.brian@gmail.com>
2022-04-16 07:24:50 +0000

    Fix from clinic

--- docker/codx-room/apps/init-repo.py
+++ docker/codx-room/apps/init-repo.py
@@ -1,22 +1,27 @@
 import json, os, logging
-
+logging.basicConfig(level=logging.INFO)
 logging.info("Initializing repository")
 
 codxjson = None
 with open('codx.json') as codx:
-  codxjson = json.loads(codx)
+  codxjson = json.load(codx)
 
 apps = codxjson.get('apps')
 if apps:
-  for k,v in apps:
-    logging.info("Installing dependencie: %s" % k)
-    os.system('codx %s' % k)
+  for app in apps.keys():
+    logging.info("Installing dependencie: %s" % app)
+    # os.system('codx %s' % app)
 
+logging.info("Updating firefox open tabs")
+url = codxjson.get('url')
 tabs = codxjson.get('tabs')
+
+with open('/usr/lib/firefox/distribution/policies.json') as ffsettings:
+  ffsettingsjson = json.load(ffsettings)
 if tabs:
-  logging.info("Updating firefox open tabs: %s" % tabs)
-  with open('/usr/lib/firefox/distribution/policies.json') as ffsettings:
-    ffsettingsjson = json.loads(ffsettings)
   ffsettingsjson["Homepage"]["Additional"] = tabs
-  with open('/usr/lib/firefox/distribution/policies.json', 'w') as ffsettings:
-    ffsettings.write(json.dumps(ffsettingsjson))
+if url:
+  ffsettingsjson["Homepage"]["URL"] = url
+
+with open('/usr/lib/firefox/distribution/policies.json', 'w') as ffsettings:
+  ffsettings.write(json.dumps(ffsettingsjson))
